ff33e77dbad5435f4a73d368d4ff03d705f2b21c
Yicheng Wang <u6610265@anu.edu.au>
2018-03-14 14:05:47 +1100

    Test printout

---
+++ rvm/src/org/Test.java
@@ -0,0 +1,10 @@
+package org;
+
+import org.jikesrvm.VM;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        VM.sysWriteln("Hello world!");
+    }
+}
