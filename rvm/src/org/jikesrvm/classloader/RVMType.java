39ff47fbbbeb4a970c64cac588c926075b894598
Yicheng Wang <u6610265@anu.edu.au>
2018-03-15 15:46:38 +1100

    Delete useless print

--- rvm/src/org/jikesrvm/classloader/RVMType.java
+++ rvm/src/org/jikesrvm/classloader/RVMType.java
@@ -254,14 +254,6 @@ public abstract class RVMType extends AnnotatedElement {
     this.classForType = classForType;
     this.dimension = dimension;
 
-    if(isClassType()){
-        num_instance_class++;
-        VM.sysWriteln("The " +  num_instance_class + " Class!");
-    }else if (isArrayType()) {
-        num_instance_array++;
-        VM.sysWriteln("The " +  num_instance_array + " Array!");
-    }
-
     /* install partial type information block (no method dispatch table) for use in type checking. */
     TIB tib = MemoryManager.newTIB(0, AlignmentEncoding.ALIGN_CODE_NONE);
     tib.setType(this);
