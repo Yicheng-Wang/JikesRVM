f740cfc92976b84e45ff6d778b62309ed9ed10d8
Yicheng Wang <u6610265@anu.edu.au>
2018-03-29 01:45:32 +1100

    Repair

--- rvm/src/org/jikesrvm/mm/mminterface/HandInlinedScanning.java
+++ rvm/src/org/jikesrvm/mm/mminterface/HandInlinedScanning.java
@@ -27,7 +27,7 @@ import org.vmmagic.pragma.Uninterruptible;
  */
 public class HandInlinedScanning {
 
-  public static final int AE_FALLBACK = 7;//(1 << AlignmentEncoding.FIELD_WIDTH) - 1;
+  public static final int AE_FALLBACK = 78;//(1 << AlignmentEncoding.FIELD_WIDTH) - 1;
   public static final int AE_REFARRAY = 6;//AE_FALLBACK - 1;
 
   public static final int AE_PATTERN_0x0  = 26;
