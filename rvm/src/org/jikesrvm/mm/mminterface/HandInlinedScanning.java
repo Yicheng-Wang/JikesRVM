4cb91430581b24f0d029c230f2de8ebd9b7c6da6
Yicheng Wang <u6610265@anu.edu.au>
2018-03-29 02:09:01 +1100

    Repair

--- rvm/src/org/jikesrvm/mm/mminterface/HandInlinedScanning.java
+++ rvm/src/org/jikesrvm/mm/mminterface/HandInlinedScanning.java
@@ -27,15 +27,15 @@ import org.vmmagic.pragma.Uninterruptible;
  */
 public class HandInlinedScanning {
 
-  public static final int AE_FALLBACK = 384;//(1 << AlignmentEncoding.FIELD_WIDTH) - 1;
-  public static final int AE_REFARRAY = 640;//AE_FALLBACK - 1;
+  public static final int AE_FALLBACK = 896;//(1 << AlignmentEncoding.FIELD_WIDTH) - 1;
+  public static final int AE_REFARRAY = 0;//AE_FALLBACK - 1;
 
   public static final int AE_PATTERN_0x0  = 128;
   public static final int AE_PATTERN_0x1  = 256;
-  public static final int AE_PATTERN_0x7  = 512;
-  public static final int AE_PATTERN_0x3F = 3;
-  public static final int AE_PATTERN_0x3  = 4;
-  public static final int AE_PATTERN_0x3D = 5;
+  public static final int AE_PATTERN_0x7  = 384;
+  public static final int AE_PATTERN_0x3F = 512;
+  public static final int AE_PATTERN_0x3  = 640;
+  public static final int AE_PATTERN_0x3D = 768;
 
   private static final int FIELD0_OFFSET =
     JavaHeader.objectStartOffset(RVMType.JavaLangObjectType) +
