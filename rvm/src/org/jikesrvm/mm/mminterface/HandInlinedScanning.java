907d138bfb516934b59c38370fa2bac36262cf7c
Yicheng Wang <u6610265@anu.edu.au>
2018-05-07 20:18:58 +1000

    restart back work

--- rvm/src/org/jikesrvm/mm/mminterface/HandInlinedScanning.java
+++ rvm/src/org/jikesrvm/mm/mminterface/HandInlinedScanning.java
@@ -27,14 +27,14 @@ import org.vmmagic.pragma.Uninterruptible;
  */
 public class HandInlinedScanning {
 
-  public static final int AE_FALLBACK = 384;//(1 << AlignmentEncoding.FIELD_WIDTH) - 1; 7
-  public static final int AE_REFARRAY = 640;//AE_FALLBACK - 1; 6
+  public static final int AE_FALLBACK = 3*(1 << (AlignmentEncoding.FIELD_WIDTH - 3));
+  public static final int AE_REFARRAY = 5*(1 << (AlignmentEncoding.FIELD_WIDTH - 3));
 
-  public static final int AE_PATTERN_0x0  = 128;
-  public static final int AE_PATTERN_0x1  = 256;
-  public static final int AE_PATTERN_0x7  = 512;
-  public static final int AE_PATTERN_0x3F = 768;
-  public static final int AE_PATTERN_0x3  = 896;
+  public static final int AE_PATTERN_0x0  = (1 << (AlignmentEncoding.FIELD_WIDTH - 3));
+  public static final int AE_PATTERN_0x1  = 2*(1 << (AlignmentEncoding.FIELD_WIDTH - 3));
+  public static final int AE_PATTERN_0x7  = 4*(1 << (AlignmentEncoding.FIELD_WIDTH - 3));
+  public static final int AE_PATTERN_0x3F = 6*(1 << (AlignmentEncoding.FIELD_WIDTH - 3));
+  public static final int AE_PATTERN_0x3  = 7*(1 << (AlignmentEncoding.FIELD_WIDTH - 3));
   public static final int AE_PATTERN_0x3D = 0;
 
   private static final int FIELD0_OFFSET =
