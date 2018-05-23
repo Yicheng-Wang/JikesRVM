96bea0797f57d49924f6484db84f878f72039dd8
Yicheng Wang <u6610265@anu.edu.au>
2018-05-23 17:10:45 +1000

    runtime

--- rvm/src/org/jikesrvm/mm/mminterface/MemoryManager.java
+++ rvm/src/org/jikesrvm/mm/mminterface/MemoryManager.java
@@ -902,6 +902,8 @@ public final class MemoryManager {
       Address first = testendpoint.plus(adjustpadding + 2*(1 << (AlignmentEncoding.FIELD_WIDTH - 3))*4);
       Address second = testendpoint.plus(adjustpadding + 4*(1 << (AlignmentEncoding.FIELD_WIDTH - 3))*4);
       VM.sysWriteln("Address now is : "+ testendpoint + " First: "+ AlignmentEncoding.getTibCodeForRegion(first) + " Second : "+ AlignmentEncoding.getTibCodeForRegion(second));
+      VM.sysWriteln(first + " First: "+ AlignmentEncoding.getTibCodeForRegion(first));
+      VM.sysWriteln(second + " Second : "+ AlignmentEncoding.getTibCodeForRegion(second));
       FirstHoles.add(first);
       SecondHoles.add(second);
     }
